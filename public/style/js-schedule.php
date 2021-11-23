53b546fac17a66e48e06e36668c48160ecb85b43
Dion Fikry Santyosa <94891379+diozhangxiu@users.noreply.github.com>
2021-11-23 15:58:33 +0700

    Delete public/style directory

--- public/style/js-schedule.php
+++
@@ -1,7 +0,0 @@
-<!-- jQuery Files -->
-        <script src="js/vendor/jquery-1.12.4.min.js"></script>
-        <script src="js/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
-        <script src="js/animate.js"></script>
-        <script src="js/custom.js"></script>
-        <script src="js/videobg.js"></script>
